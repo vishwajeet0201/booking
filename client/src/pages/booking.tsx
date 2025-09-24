a35f5e1b7442536c0b6978713cbd480abba77aae
vishwajeet0201 <vishwajeetind0201@gmail.com>
2025-09-24 19:58:23 +0530

    Delete client directory

--- client/src/pages/booking.tsx
+++
@@ -1,9 +0,0 @@
-import { BookingForm } from "@/components/booking/BookingForm";
-
-export default function BookingPage() {
-  return (
-    <div className="min-h-screen bg-background">
-      <BookingForm />
-    </div>
-  );
-}
